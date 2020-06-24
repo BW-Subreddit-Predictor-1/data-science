d9b9a31605ed8f8808857c5b19a31bb58fe89720
zachary-hamilton <57552986+zachary-hamilton@users.noreply.github.com>
2020-06-24 19:59:42 -0400

    added print url to test script

--- testing_predict_subreddit_route.py
+++ testing_predict_subreddit_route.py
@@ -8,5 +8,6 @@ url = "https://bwptphsp1ds.herokuapp.com/predict_subreddit"  # if you want to te
 # this assumes that the agreed upon json keys are `title` and 'body' 
 val = {'title': 'i want to learn data science', 'body': 'b'} 
 r_success = requests.post(url, data=json.dumps(val))
+print(f'url: {url}')
 print(f"request responded: {r_success}.\nthe content of the response was {r_success.json()}")
 # you'll get a 200 response if the keys align, and something bad if the keys don't align. 
